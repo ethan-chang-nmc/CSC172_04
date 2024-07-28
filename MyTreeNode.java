ca003c57f9e5407df7424fbad9c409d6395d348a
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2024-07-28 13:09:44 -0400

    Create MyTreeNode.java

---
+++ MyTreeNode.java
@@ -0,0 +1,13 @@
+/*
+*Ethan Chang
+*echang28@u.rochester.edu
+*
+*Partner: Darron King
+*
+*07/28/2024: Creating Binary Search Trees
+*/
+public class MyTreeNode <T extends Comparable<T>>
+{
+ T data ;
+ MyTreeNode<T> leftchild, rightchild, parent;
+}
