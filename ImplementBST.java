f8710b941bdeffc43207625fbf767b51ce3beed1
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2024-07-28 19:05:49 -0400

    Create ImplementBST.java

---
+++ ImplementBST.java
@@ -0,0 +1,20 @@
+/*
+*Ethan Chang
+*echang28@u.rochester.edu
+*
+*Partner: Darron King
+*
+*07/28/2024: Creating Binary Search Trees
+*/
+public class MyBSTImpl<T extends Comparable<T>> implements MyBST<T>
+{
+  private MyTreeNode<T> root = null; //create root node
+
+  /*insert method calling on recursive insert*/
+  public void insert(T x) 
+  {
+    root = insertRec(root, x, null);
+  }
+
+  
+}
