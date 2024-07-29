ccc703adabc93090a5b0f0101b92fd8587fb274c
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2024-07-28 21:59:55 -0400

    Update MyTreeNode.java

--- MyTreeNode.java
+++ MyTreeNode.java
@@ -67,21 +67,72 @@ public class MyTreeNode <T extends Comparable<T>>
   /*recursive lookup method*/
   private boolean lookupRec(MyTreeNode<T> node, T x) 
   {
-    if (node == null) 
+    if (node == null)
     {
-      return false;
+      return false; //return false if null
     }
     if (x.compareTo(node.data) == 0) 
     {
-      return true;
+      return true; //returns true if node is found
     } 
     else if (x.compareTo(node.data) < 0) 
     {
-      return lookupRec(node.leftchild, x);
+      return lookupRec(node.leftchild, x); //continue traversing left if less than current node
     } 
     else 
     {
-      return lookupRec(node.rightchild, x);
+      return lookupRec(node.rightchild, x); //continue traversing right if greater than current node
     }
+  }
+
+  /*recursive delete method*/
+  private MyTreeNode<T> deleteRec(MyTreeNode<T> node, T x) 
+  {
+    if (node == null) 
+    {
+      return null; //check for null node
+    }
+    if (x.compareTo(node.data) < 0) 
+    {
+      node.leftchild = deleteRec(node.leftchild, x); //traverse left if less than current node
+    } 
+    else if (x.compareTo(node.data) > 0) 
+    {
+      node.rightchild = deleteRec(node.rightchild, x); //traverse right if greater than current node
+    } 
+    else //node found
+    {
+      if (node.leftchild == null && node.rightchild == null) //case 1: leaf node, sets it to null
+      {
+        return null; 
+      } 
+      else if (node.leftchild == null) //cases 2: one child right
+      {
+        node.rightchild.parent = node.parent;  //sets right child's parent to be current nodes parent
+        return node.rightchild; //sets current node to right child, removes current node
+      } 
+      else if (node.rightchild == null) //cases 2: one child left
+      {
+        node.leftchild.parent = node.parent; //sets left child's parent to be current nodes parent
+        return node.leftchild; //sets current node to left child, removes current node
+      } 
+      else //case 3: two children
+      {
+        MyTreeNode<T> successor = findMin(node.rightchild); //sets successor to be minimum of right side current node
+        node.data = successor.data; //set current nodes data to be equal to successor
+        node.rightchild = deleteRec(node.rightchild, successor.data); //removes the successor node
+      }
+    }
+    return node;
+  }
+
+  /*find minimum of current node to use in delete function*/
+  private MyTreeNode<T> findMin(MyTreeNode<T> node) 
+  {
+    while (node.leftchild != null) 
+    {
+      node = node.leftchild; //continues traversing left to find minimum
+    }
+    return node;
   }
 }
